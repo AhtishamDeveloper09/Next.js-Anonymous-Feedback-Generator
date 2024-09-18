574659370f8d68cfd1ed81d747b6368ef858a270
AhtishamDeveloper09 <Ahtishamprine@gmail.com>
2024-09-18 23:03:00 +0500

    fullstack anonymous feedback generator app project added

---
+++ src/model/User.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const MessageSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const UserScheme = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolen,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolen,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
